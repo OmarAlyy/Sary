f5cfd82482c805e23e6b78cf91d75c283c672d0c
omaraly <omar.aly169@gmail.com>
2021-08-08 03:15:40 +0200

    Remove all build folders and Remove all unused

--- app/build/generated/source/dataBinding/trigger/debug/com/omar_aly/sary/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package com.omar_aly.sary;
-
-import androidx.databinding.BindingBuildInfo;
-
-@BindingBuildInfo
-public class DataBindingInfo {}
--- app/build/generated/source/kapt/debug/com/omar_aly/sary/BR.java
+++
@@ -1,9 +0,0 @@
-package com.omar_aly.sary;
-
-public class BR {
-  public static final int _all = 0;
-
-  public static final int catalog = 1;
-
-  public static final int viewModel = 2;
-}
